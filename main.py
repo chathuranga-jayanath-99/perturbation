8acd260ed1a2b1c06156174d6a46bb8493fb27a6
chaturannga-jayanath <chathuranga.19@cse.mrt.ac.lk>
2023-12-05 20:06:11 +0530

    implement main to list files

---
+++ main.py
@@ -0,0 +1,11 @@
+import os 
+
+samples_dir_path = './samples/'
+perturbed_samples_dir_path = './perturbed_samples/'
+corrupted_samples_dir_path = './corrupted_samples/'
+
+execution_command = 'timeout 600 java -jar ./perturbation_model/target/perturbation-0.0.1-SNAPSHOT-jar-with-dependencies.jar '
+
+if __name__ == '__main__':
+    with os.scandir(samples_dir_path) as entries:
+        print(entries)
