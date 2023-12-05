c8c5b6a061e77d19877b1a5ea68c5bbae09f8238
chaturannga-jayanath <chathuranga.19@cse.mrt.ac.lk>
2023-12-05 23:10:19 +0530

    mv perturbed file to another dir

--- main.py
+++ main.py
@@ -12,8 +12,12 @@ base_command = 'timeout 600 java -jar ./perturbation_model/target/perturbation-0
 if __name__ == '__main__':
     with os.scandir(samples_dir_path) as files:
         for file in files:
+            # generate perturbations 
             if (file.name.endswith('.java')):
                 print(file.name)
                 file_path = samples_dir_path + file.name
                 execution_command = f'{base_command} {file_path} {arg_buglab}'
-                os.system(execution_command)        
+                os.system(execution_command)   
+
+                os.system(f'mv ./{file.name.split(.)[0]}Perturbation.java ./../perturbed_samples/{file.name.split(.)[0]}Perturbation.java')
+    # read perurb file and generate corrupt file
