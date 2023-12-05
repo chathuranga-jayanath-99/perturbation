8ba998ed6cecef2b1bcaf3db40941d2fbe723406
chaturannga-jayanath <chathuranga.19@cse.mrt.ac.lk>
2023-12-06 00:58:34 +0530

    generate corrupt file

--- main.py
+++ main.py
@@ -41,29 +41,28 @@ if __name__ == '__main__':
                 with open(original_file_path) as of:
                     original_file_lines = of.readlines()
 
-
                 with open(perturbed_file_path) as pf:
                     perturbed_file_lines = pf.readlines()
                     print(f'perturbed_file_lines: {perturbed_file_lines}')
                     
-                    # for perturbed_file_line in perturbed_file_lines:
-                    perturbed_file_line = perturbed_file_lines[0]
-                    perturbed_file_infos = perturbed_file_line.split('^')
+                    for i in range(len(perturbed_file_lines)):
+                        perturbed_file_line = perturbed_file_lines[i]
+                        perturbed_file_infos = perturbed_file_line.split('^')
 
-                    action = perturbed_file_infos[0]
-                    corrupt_line_code = perturbed_file_infos[1]
-                    corrupt_line_no = int(perturbed_file_infos[2]) - 1
-                    
-                    original_line_code = original_file_lines[corrupt_line_no]
-                    spaces_match = re.match(r'^\s*', original_line_code)
-                    original_line_spaces_count = len(spaces_match.group(0))
+                        action = perturbed_file_infos[0]
+                        corrupt_line_code = perturbed_file_infos[1]
+                        corrupt_line_no = int(perturbed_file_infos[2]) - 1
+                        
+                        original_line_code = original_file_lines[corrupt_line_no]
+                        spaces_match = re.match(r'^\s*', original_line_code)
+                        original_line_spaces_count = len(spaces_match.group(0))
 
-                    corrupt_line_code = ' '*original_line_spaces_count + corrupt_line_code + '\n'
+                        corrupt_line_code = ' '*original_line_spaces_count + corrupt_line_code + '\n'
 
-                    corrupt_file_lines = original_file_lines[:]
-                    corrupt_file_lines[corrupt_line_no] = corrupt_line_code
+                        corrupt_file_lines = original_file_lines[:]
+                        corrupt_file_lines[corrupt_line_no] = corrupt_line_code
 
-                    print(corrupt_file_lines)
-                    corrupt_file_path = corrupt_dir_path + '/1.java'
-                    with open(corrupt_file_path, 'w') as cf:
-                        cf.writelines(''.join(corrupt_file_lines))
+                        print(corrupt_file_lines)
+                        corrupt_file_path = corrupt_dir_path + f'/{i+1}.java'
+                        with open(corrupt_file_path, 'w') as cf:
+                            cf.writelines(''.join(corrupt_file_lines))
