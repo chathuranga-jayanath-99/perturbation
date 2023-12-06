beabae40b904fbb1cbef2153b64a5fc5e042abd9
chaturannga-jayanath <chathuranga.19@cse.mrt.ac.lk>
2023-12-06 14:07:28 +0530

    change arg for perturbing

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
             if (file.name.endswith('.java')):
                 print(file.name)
                 file_path = samples_dir_path + file.name
-                execution_command = f'{base_command} {file_path} {arg_buglab}'
+                execution_command = f'{base_command} {file_path} {arg_selfapr}'
                 os.system(execution_command)   
 
                 filname_without_ext = file.name.split('.')[0]
